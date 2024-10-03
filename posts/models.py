3cf66d780a03dbf1eb1831e6f20340572c6fd3e0
ndiaz <nohemijdiaz@gmail.com>
2024-10-03 18:17:26 -0400

    model added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class publication(models.Model):
+    text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.text[:50]
