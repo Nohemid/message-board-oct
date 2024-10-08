c3388a9c56dbdec4ac39f846ff083d1a65597ccf
ndiaz <nohemijdiaz@gmail.com>
2024-10-08 18:15:37 -0400

    post list added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import publication
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    model = publication
+    template_name = "post_list.html"
